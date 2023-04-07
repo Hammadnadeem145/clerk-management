64a40a97d6bdb2e6e0888c023fb9caf300c656de
Hammad Nadeem <hammadnadeem145@gmail.com>
2023-04-07 21:29:22 +0500

    clerk-nectjs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ClerkProvider, SignedIn, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider>
+      <SignedIn>
+        <UserButton />
+        <Component {...pageProps} />
+      </SignedIn>
+      <SignedOut>
+        <SignIn />
+      </SignedOut>
+    </ClerkProvider>
+  );
 }
